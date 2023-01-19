8853f33a80e5815e4a8ef83f84ec96ed77ca7b95
losadas <losadas@uninorte.edu.co>
2023-01-19 15:18:01 -0500

    countryf

--- part1-react/country-data/src/App.js
+++ part1-react/country-data/src/App.js
@@ -1,7 +1,6 @@
 import { Fragment, useEffect, useState } from "react";
 
 function App() {
-
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
       .then((response) => response.json())
@@ -10,51 +9,30 @@ function App() {
       });
   }, []);
 
-
-
-
   const [data, setData] = useState([]);
   const [newFilter, setNewFilter] = useState("");
   const [isClicked, setIsClicked] = useState(false);
-  const [position, setPosition] = useState(0)
+  const [position, setPosition] = useState(0);
 
-
-
-
-  const datafilter = data
-  .filter((element) => {
-    return element.name.common
-      .toLowerCase()
-      .includes(newFilter.toLowerCase());
-  })
-
-
-
+  const datafilter = data.filter((element) => {
+    return element.name.common.toLowerCase().includes(newFilter.toLowerCase());
+  });
 
   const handle = (e) => {
     setNewFilter(e.target.value);
     setIsClicked(false);
   };
 
-
-
-
-  const show =
-    datafilter.length < 11 &&
-    datafilter.length > 1;
-
-
+  const show = datafilter.length < 11 && datafilter.length > 1;
 
   const handleClick = (e) => {
     setIsClicked(true);
-    setPosition(e.target.id)
+    setPosition(e.target.id);
   };
 
   const lang = () => {
     return Object.values(datafilter[position].languages);
-  }
-  
-
+  };
 
   return (
     <>
@@ -63,19 +41,18 @@ function App() {
       <br />
       {newFilter === "" && <p>Type to search</p>}
       {show &&
-        datafilter
-          .map((element, index) => {
-            return (
-              <Fragment key={element.name.common}>
-                <label>{element.name.common}</label>
-                <button onClick={handleClick} id={index}>
-                  Show
-                </button>
-                <br />
-              </Fragment>
-            );
-          })} 
-      {isClicked && 
+        datafilter.map((element, index) => {
+          return (
+            <Fragment key={element.name.common}>
+              <label>{element.name.common}</label>
+              <button onClick={handleClick} id={index}>
+                Show
+              </button>
+              <br />
+            </Fragment>
+          );
+        })}
+      {isClicked && (
         <>
           <h2>{datafilter[position].name.common}</h2>
           <p>Capital: {datafilter[position].capital}</p>
@@ -83,27 +60,29 @@ function App() {
           <h3>Languages</h3>
           <ul>
             {lang().map((element, index) => {
-              return <li key={index}>{element}</li>
+              return <li key={index}>{element}</li>;
             })}
           </ul>
-          <img src={`${datafilter[position].flags.png}`} alt="img flag"/>
+          <img src={`${datafilter[position].flags.png}`} alt="img flag" />
         </>
-        }
-      {datafilter.length > 10 &&
-        newFilter !== "" && <p>Too many matches, specify another filter</p>}
-      {datafilter.length === 1 &&
+      )}
+      {datafilter.length > 10 && newFilter !== "" && (
+        <p>Too many matches, specify another filter</p>
+      )}
+      {datafilter.length === 1 && (
         <>
-        <h2>{datafilter[0].name.common}</h2>
-        <p>Capital: {datafilter[0].capital}</p>
-        <p>Population: {datafilter[0].population}</p>
-        <h3>Languages</h3>
-        <ul>
-          {lang().map((element, index) => {
-            return <li key={index}>{element}</li>
-          })}
-        </ul>
-        <img src={`${datafilter[0].flags.png}`} alt="img flag"/>
-      </>}
+          <h2>{datafilter[0].name.common}</h2>
+          <p>Capital: {datafilter[0].capital}</p>
+          <p>Population: {datafilter[0].population}</p>
+          <h3>Languages</h3>
+          <ul>
+            {lang().map((element, index) => {
+              return <li key={index}>{element}</li>;
+            })}
+          </ul>
+          <img src={`${datafilter[0].flags.png}`} alt="img flag" />
+        </>
+      )}
     </>
   );
 }
